b4f4ddd5c07454cc20dd8886ec034fa8cf0c6421
mahfuz091 <engmahfuz14@gmail.com>
2023-02-10 21:20:34 +0600

    ff

--- js/event.js
+++ js/event.js
@@ -1,1 +1,32 @@
 console.log('dom dom');
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = function (){
+    document.body.style.backgroundColor = 'purple';
+}
+
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+const makeGreenButton = document.getElementById('make-green');
+console.log(makeGreenButton)
+makeGreenButton.addEventListener("click", function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
